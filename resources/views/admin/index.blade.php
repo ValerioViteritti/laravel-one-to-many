a73c5844026fe021c98e5da1dc7aadaf01197ddd
ValerioViteritti <vviteritti@gmail.com>
2024-09-26 17:11:15 +0200

    fix:minor

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,5 +2,5 @@
 
 @section('content')
     <h1>Benvenuti nella dashboard di Boolfolio</h1>
-    <h3>Attualmente sono presenti {{ $count_posts }} post</h3>
+    <h3>Attualmente sono presenti {{ $count_posts }} post da leggere</h3>
 @endsection
