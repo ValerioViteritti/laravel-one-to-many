17248de24bf7f38fa37936dccb8221e6c9962e59
ValerioViteritti <vviteritti@gmail.com>
2024-09-30 15:46:10 +0200

    fix: index

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,4 +3,5 @@
 @section('content')
     <h1>Benvenuti nella dashboard di Boolfolio</h1>
     <h3>Attualmente sono presenti {{ $count_posts }} post da leggere</h3>
+    <h4>Many to Many</h4>
 @endsection
